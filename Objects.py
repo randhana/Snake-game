7c18e73c1e1bc8f00a1f7e5ac47e7cfe65f16708
randhana <pulathisi1921@gmail.com>
2021-06-06 21:49:09 +0530

    Adding Apple class

--- Objects.py
+++ Objects.py
@@ -12,4 +12,22 @@ class MovableObject(StaticObject):
 
   def __init__(self, tag, x, y, direction):
     super().__init__(tag, x, y)
-    self._direction = direction
+    self._direction = direction 
+    
+
+class Apple(StaticObject):
+
+  def __init__(self, tag, x, y):
+    super().__init__(tag, x, y)
+    self._value = random.randint(0, 10)
+
+    if self._value < 3:
+      self._color = (255, 0, 0)
+    elif self._value >= 3 and self._value < 6:
+      self._color = (0, 255, 0)
+    elif self._value >= 6 and self._value <= 9:
+      self._color = (0, 0, 255)
+    elif self._value == 10:
+      self._color = (255, 235, 0)
+
+
